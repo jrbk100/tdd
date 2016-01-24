dacdc1e505c4aca9c115a519097eba62e3e4fda5
James Knowling <james.knowling@trinitymirror.com>
2016-01-24 22:37:02 +0000

    Added .gitignore

---
+++ src/main/java/tdd/TestingSpock.java
@@ -0,0 +1,11 @@
+package tdd;
+
+/**
+ * Created by JamesKnowling on 24/01/2016.
+ */
+public class TestingSpock {
+
+  public static String returnSomething() {
+    return "Hello";
+  }
+}
---
+++ src/test/groovy/tdd/test1Spec.groovy
@@ -0,0 +1,13 @@
+package tdd
+
+import spock.lang.Specification
+
+/**
+ * Created by JamesKnowling on 24/01/2016.
+ */
+class test1Spec extends Specification {
+
+  def "one should always say hello"() {
+    Test1.re
+  }
+}
